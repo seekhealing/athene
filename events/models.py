4a35a96cca7d39b6d1e1238020b1db76b71fd9d2
Joshua "jag" Ginsberg <jag@flowtheory.net>
2019-03-10 17:30:46 -0400

    Fix day of week labels.

--- events/models.py
+++ events/models.py
@@ -11,13 +11,13 @@ from multiselectfield import MultiSelectField
 from . import google, twilio
 
 DAYS_OF_WEEK = [
-    (0, 'Monday'),
-    (1, 'Tuesday'),
-    (2, 'Wednesday'),
-    (3, 'Thursday'),
-    (4, 'Friday'),
-    (5, 'Saturday'),
-    (6, 'Sunday')
+    (0, 'Sunday'),
+    (1, 'Monday'),
+    (2, 'Tuesday'),
+    (3, 'Wednesday'),
+    (4, 'Thursday'),
+    (5, 'Friday'),
+    (6, 'Saturday')
 ]
 
 class Calendar(models.Model):
