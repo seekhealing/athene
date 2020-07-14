6e58c4440eb6f924050d11fdddb675ddd9fdedbc
Joshua "jag" Ginsberg <jag@flowtheory.net>
2020-07-13 20:27:42 -0400

    Enable debug logging.

--- athene/settings.py
+++ athene/settings.py
@@ -125,9 +125,7 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {"standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"}},
-    "handlers": {
-        "default": {"level": "DEBUG" if DEBUG else "INFO", "formatter": "standard", "class": "logging.StreamHandler"},
-    },
+    "handlers": {"default": {"level": "DEBUG", "formatter": "standard", "class": "logging.StreamHandler"}},
     "loggers": {
         "athene": {"handlers": ["default"], "level": "DEBUG"},
         "seekers": {"handlers": ["default"], "level": "DEBUG"},
